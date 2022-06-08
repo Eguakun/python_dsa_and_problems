340cf7d908a9a7c5044f9ce492ea532a4fcc8776
Eguakun <meguakun@my.tnstate.edu>
2022-06-08 15:42:13 -0500

    add two numbers

---
+++ algorithms_and_algorithmic_techniques/linked_list_problems/add_two_numbers.py
@@ -0,0 +1,30 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        positionToInsert = ListNode()
+        cur = positionToInsert
+
+        carry = 0
+        # if carry is nonnull we want to continue the loop
+        # in edge case where we still have carry at the end of iteration, digit1 and digit2 will be null and we will still acocount for the carry
+        while l1 or l2 or carry:
+            digitOfList1 = l1.val if l1 else 0
+            digitOfList2 = l2.val if l2 else 0
+
+            # new digit
+            val = digitOfList1 + digitOfList2 + carry
+            carry = val // 10
+            val = val % 10 # to get the ones place
+            cur.next = ListNode(val)
+
+            # update our pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return positionToInsert.next
+
+
+
